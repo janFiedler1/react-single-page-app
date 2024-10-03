33b17352171a9bdcce3f1d4b0370f1e804629677
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2024-10-03 11:23:23 -0400

    styling and tags in PieceComponent

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Piece from './PieceComponent';
+import '../component-css/HomeComponent.css';
 //import myImage from 'rockets-shorts-enhanced-png.png';
 //<img src={myImage}/>
 
-const pieces = [{id: 1, title: 'Rockets Shorts', path: process.env.PUBLIC_URL+'/rockets-shorts-enhanced.png', type: "shorts"},
-    {id: 2, title: 'Earth First Longsleeve', path: process.env.PUBLIC_URL+'/earth-first-longsleeve.png', type: "shorts"},
-    {id: 3, title: 'Rockets Shorts', path: process.env.PUBLIC_URL+'/rockets-shorts-enhanced-png.png', type: "shorts"},
-    {id: 4, title: 'Rockets Shorts', path: process.env.PUBLIC_URL+'/rockets-shorts-enhanced-png.png', type: "shorts"},
+const pieces = [{id: 1, title: 'Rockets Shorts', path: process.env.PUBLIC_URL+'/rockets-shorts-enhanced.png', type: "shorts", payment_link:"https://buy.stripe.com/test_8wMg2pbRE51r64wfYZ"},
+    {id: 2, title: 'Earth First Longsleeve', path: process.env.PUBLIC_URL+'/earth-first-longsleeve.png', type: "shorts", payment_link:"https://buy.stripe.com/test_8wMg2pbRE51r64wfYZ"},
+    {id: 3, title: 'Rockets Shorts', path: process.env.PUBLIC_URL+'/rockets-shorts-enhanced-png.png', type: "shorts", payment_link:"https://buy.stripe.com/test_8wMg2pbRE51r64wfYZ"},
+    {id: 4, title: 'Rockets Shorts', path: process.env.PUBLIC_URL+'/rockets-shorts-enhanced-png.png', type: "shorts", payment_link:"https://buy.stripe.com/test_8wMg2pbRE51r64wfYZ"},
  ]
 
 function Home () {
@@ -14,7 +15,7 @@ function Home () {
         <React.Fragment>
             <div className="pieces-container">
                 {pieces.map((piece) => (
-                    <Piece key={piece.key} title={piece.title} path={piece.path}/>
+                    <Piece key={piece.key} title={piece.title} path={piece.path} payment_link={piece.payment_link}/>
                 ))}
             </div>    
         </React.Fragment>
--- src/components/PieceComponent.js
+++ src/components/PieceComponent.js
@@ -1,14 +1,21 @@
 import React from "react";
 import '../component-css/PieceComponent.css';
 
+/**
+ * @param {int} key - unique identifier
+ * @param {string} title - name of the piece
+ * @param {url} path - path to the image
+ * @param {url} payment_link - payment link
+ */
 function Piece(prop) {
     return (
         <React.Fragment>
-            
-            <div key={prop.key} className="piece-container">
-                <div>{prop.title}</div>
-                <img src={prop.path}/>  
-            </div>
+            <a href={prop.payment_link}>
+                <div key={prop.key} className="piece-container">
+                    <div>{prop.title}</div>
+                    <img src={prop.path}/>  
+                </div>
+            </a>
         </React.Fragment>
     )
 }
